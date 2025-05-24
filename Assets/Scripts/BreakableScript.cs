e15ccb4799d78c79b3226387bc2a82bcf07a3530
TheUndeadElite <almir.bajraktarevic@elev.ga.lbs.se>
2025-05-24 16:40:00 +0200

    Breakable box

--- Assets/Scripts/BreakableScript.cs
+++ Assets/Scripts/BreakableScript.cs
@@ -1,6 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class BreakableScript : MonoBehaviour
+public class BreakableScript : MonoBehaviour, IInteractable
 {
-    
+    public GameObject originalObject;
+    public GameObject fracturedObject;
+    public float explosionMinForce = 5;
+    public float explosionMaxForce = 100;
+    public float explosionForceRadius = 10;
+    public float fragScaleFactor = 1;
+
+    AudioSource audioSource;
+    public AudioClip ExplosionSFX;
+    [SerializeField] List<Rigidbody> rbs;
+    public void Interact()
+    {
+        Explode();
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Explode()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+
+        audioSource.PlayOneShot(ExplosionSFX);
+
+        foreach (Rigidbody piece in rbs)
+        {
+            piece.isKinematic = false;
+            piece.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionForceRadius);
+        }
+    }
 }
